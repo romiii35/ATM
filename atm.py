1ded7fc3dbd5bdb038116be49b4f14eeb3d95cb7
romiii35 <romiii9735@gmail.com>
2025-07-18 13:25:05 +0900

    ✨ 영수증 보기 기능 추가
    사용자가 영수증 내역을 확인할수 있는 기능 추가 및 영수증 내역 없을 시 안내 메시지 작성

--- atm.py
+++ atm.py
@@ -6,7 +6,8 @@
 # 사용 기능 입력을 받는 기능을 구현해주세요.(무한정 계속 떠 있게- 사람들이 시도때도 없이 많이온다.)
 
 balance = 1000000
-
+# 초기화 : 사용하기 위해 미리 만드는것, 초기화 특정값을 넣어서 만들거나, 빈 값을 넣어서 만듭니다.(제일 위에 만드는게 좋다, 발란스 뒤에 오는게 좋다. while뒤도 상관없지만, 매번 초기화되기에 불가//...)
+receipts = [] #[('입금', balance, deposit_amount),'출금', balance, withdraw_amount ] # 일어나는 순간에 데이터를 넣어야한다..
 #무한루프?  while vs for
 # while문과 for문
 # while문은 조건을 달성할때까지(달성지점 필요), for문 횟수에 제한을 두고 시작
@@ -23,15 +24,24 @@ while True: #True이기에 무조건 실행-무한
         deposit_amount = int(input('입금할 금액을 입력해주세요: ')) #str:5000 -> int
         balance += deposit_amount #balance = balance + deposit_amount
         print(f'입금하신 금액은 {deposit_amount}원이고, 현재 잔액은{balance}원 입니다.')
-    
+        receipts.append(('입금', deposit_amount, balance))  #보안!!!!!! ->'입금' 입금한 금액(deposit_amount) 보안!!->튜플로 만들기
+   
     if num == '2':
         withdraw_amount = int(input('출금할 금액을 입력해주세요:'))
         withdraw_amount = min(balance, withdraw_amount)
         balance -= withdraw_amount #balance = balance + withdraw_amount
         print(f'출금하신 금액은 {withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+        receipts.append(('출금', withdraw_amount, balance))
     
     if num == '3':
-        pass
+        if receipts:
+        # [('입금', 3000, 13000), ('출금', 5000, 8000), ]
+            for i in receipts:
+            # i = ('입금', 3000, 13000)
+            # i[0] => '출금'
+                print(f"{i[0]}: {i[1]}원 | 잔액 : {i[2]}")
+        else:
+            print("영수증 내역이 없습니다.")      
      
 print(f'서비스를 종료합니다.현재 잔액은 {balance}원 입니다.') # break 전, 입력가능
 
